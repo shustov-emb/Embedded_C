23482044ea98c816b17a2042883acccf74aa1f91
shustov-emb <raf_9009@mail.ru>
2026-03-25 17:26:33 +0900

    Правки в readme и в программах, убрал sleep

--- homework_11/src/mutex.c
+++ homework_11/src/mutex.c
@@ -1,15 +1,15 @@
 /**
  * @file mutex.c
  * @author Шустов Александр
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2026-03-25
- * 
+ *
  * @copyright Copyright (c) 2026
- * 
+ *
  */
 
-#define _POSIX_C_SOURCE 200112L //без этой строчки не определялся pthread_spinlock_t
+#define _POSIX_C_SOURCE 200112L // без этой строчки не определялся pthread_spinlock_t
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -35,78 +35,82 @@
  * thread_id - id потоков
  */
 void *status;
-pthread_mutex_t locks[STORE_COUNT]; 
+pthread_mutex_t locks[STORE_COUNT];
 pthread_t threads[4];
 int thread_id[4] = {0, 1, 2, 3};
 
 /**
  * @brief Данные для инициализации
  */
-int store_min = 900;            //Минимальное значение запасов в магазине на старте
-int store_max = 1100;           //Максимальное количество запасов в магазине на старте
-int store_supply[STORE_COUNT];  //Массив с запасами магазинов
+int store_min = 900;           // Минимальное значение запасов в магазине на старте
+int store_max = 1100;          // Максимальное количество запасов в магазине на старте
+int store_supply[STORE_COUNT]; // Массив с запасами магазинов
 
-int customer_min = 9900;             //Минимальное значение запасов покупателя на старте
-int customer_max = 10100;            //Максимальное количество запасов покупателя на старте
-int customer_demand[CUSTOMER_COUNT]; //Массив с потербностями покупателей
+int customer_min = 9900;             // Минимальное значение запасов покупателя на старте
+int customer_max = 10100;            // Максимальное количество запасов покупателя на старте
+int customer_demand[CUSTOMER_COUNT]; // Массив с потербностями покупателей
 
 /**
  * @brief Поточная функция скупки товаров из магазинов
- * 
- * @param [in] arg Параметры потока, в нашем случае число - id потока 
- * @return void* 
+ *
+ * @param [in] arg Параметры потока, в нашем случае число - id потока
+ * @return void*
  */
 void *BuyGoods(void *arg)
 {
-    
-    int thread_id = *(int *)arg; //Получем идентификатор потока назначенный при создании
+
+    int thread_id = *(int *)arg; // Получем идентификатор потока назначенный при создании
     while (1)
     {
-        //Цикл для прохода покупателя по магазинам, по очерёдно
+        // Цикл для прохода покупателя по магазинам, по очерёдно
         for (size_t i = 0; i < STORE_COUNT; i++)
         {
 
-            //Пытаемся заблокировать магазин
+            // Пытаемся заблокировать магазин
             if (!pthread_mutex_trylock(&locks[i]))
             {
                 printf("Customer %d (%d)\t| store %ld (%d)\t\t| -> ENTER\n", thread_id, customer_demand[thread_id], i, store_supply[i]);
 
-                //Проверяем если в магазине нет припасов, то выходим из него, предварительно разблокировав
+                // Проверяем если в магазине нет припасов, то выходим из него, предварительно разблокировав
                 if (store_supply[i] <= 0)
                 {
                     printf("Customer %d (%d)\t| store %ld (%d)\t\t| <- LEAVE, STORE EMPTY\n", thread_id, customer_demand[thread_id], i, store_supply[i]);
                     pthread_mutex_unlock(&locks[i]);
+                    // printf("Customer %d\t\t| GOING TO SLEEP zzzZZZ\n", thread_id);
+                    // sleep(2);
                     continue;
                 }
-                
-                /*Если в магазине есть какой нибудь товар, то супаем его/
-                 *Сделал циклом, а не просто вычитанием, хотя бы какую-то полезную нагрузку дать потоку */
-                int temp = store_supply[i];
-                for (int j = 0; j < temp; j++)
+                else
                 {
-                    //Если потребности покупателя удовлетворены, мы разблокируем магазин и выходим из функции
-                    if (customer_demand[thread_id] <= 0)
+                    /*Если в магазине есть какой нибудь товар, то супаем его/
+                     *Сделал циклом, а не просто вычитанием, хотя бы какую-то полезную нагрузку дать потоку */
+                    int temp = store_supply[i];
+                    for (int j = 0; j < temp; j++)
                     {
-                        pthread_mutex_unlock(&locks[i]);
-                        return 0;
+                        // Если потребности покупателя удовлетворены, мы разблокируем магазин и выходим из функции
+                        if (customer_demand[thread_id] <= 0)
+                        {
+                            pthread_mutex_unlock(&locks[i]);
+                            return 0;
+                        }
+
+                        customer_demand[thread_id]--;
+                        store_supply[i]--;
                     }
 
-                    customer_demand[thread_id]--;
-                    store_supply[i]--;
+                    // Если потребности клиента не были утолены, то разблокируем магазин и идём в другой
+                    printf("Customer %d (%d)\t| store %ld (%d)\t\t| + BUY\n", thread_id, customer_demand[thread_id], i, store_supply[i]);
+                    pthread_mutex_unlock(&locks[i]);
                 }
-
-                // Если потребности клиента не были утолены, то разблокируем магазин и идём в другой
-                printf("Customer %d (%d)\t| store %ld (%d)\t\t| + BUY\n", thread_id, customer_demand[thread_id], i, store_supply[i]);
-                pthread_mutex_unlock(&locks[i]);
             }
-            //Если магазин занят, то так и пишем
+            // Если магазин занят, то так и пишем
             else
             {
                 printf("Customer %d\t\t| store %ld\t\t| x STORE BUSY\n", thread_id, i);
             }
 
-            //После похода в магазин поток засыпает на 2 секунды
-            sleep(2);
+            // printf("Customer %d\t\t| GOING TO SLEEP zzzZZZ\n", thread_id);
+            // sleep(2);
         }
     }
 }
@@ -114,7 +118,7 @@ void *BuyGoods(void *arg)
 /**
  * @brief Вспомогательная функция для разблокировки мьютекса
  * Нужна для pthread_cleanup_push
- * @param [in] arg Параметр с pthread_mutex_t  
+ * @param [in] arg Параметр с pthread_mutex_t
  */
 void cleanup_mutex_unlock(void *arg)
 {
@@ -123,21 +127,21 @@ void cleanup_mutex_unlock(void *arg)
 
 /**
  * @brief Функция погрузчика для загрузки товаров в магазины
- * 
- * @return void* 
+ *
+ * @return void*
  */
 void *LoadShops()
 {
     while (1)
     {
-        //Выбираем рандомно в какой магазин поток будет грузить припасы
+        // Выбираем рандомно в какой магазин поток будет грузить припасы
         int store_to_supply = rand() % 5;
 
-        //Пытаемася зайти в магазин
+        // Пытаемася зайти в магазин
         if (!pthread_mutex_trylock(&locks[store_to_supply]))
         {
             /*Данный поток прерывается извне, и ближайшее прерывание происходит на pritnf
-            Мы просто не успеем дойти до pthread_mutex_unlock и выходим до освобождения мютекса, 
+            Мы просто не успеем дойти до pthread_mutex_unlock и выходим до освобождения мютекса,
             и именно поэтому мы пользуемся pthread_cleanup_push.
             Этот макрос позволяет нам гарантированно выполнить все что находиться от pthread_cleanup_push и до pthread_cleanup_pop
             и вызвать функцию разблокировки мьютекса которую мы передали*/
@@ -145,7 +149,7 @@ void *LoadShops()
             store_supply[store_to_supply] += LOADER_SUPPLY_AMOUNT;
             printf("Loader\t\t\t| store %d (%d)\t\t| ++ LOAD SUCCESS\n", store_to_supply, store_supply[store_to_supply]);
             pthread_cleanup_pop(1);
-            //pthread_mutex_unlock(&locks[store_to_supply]);
+            // pthread_mutex_unlock(&locks[store_to_supply]);
         }
         // Если зайти в магазин не получилось, пишем что он занят
         else
@@ -166,22 +170,22 @@ void Init()
 {
     srand(time(NULL));
 
-    //Рандомим количество товаров в каждом магазине исоздаём мьютексы
+    // Рандомим количество товаров в каждом магазине исоздаём мьютексы
     for (size_t i = 0; i < STORE_COUNT; i++)
     {
         store_supply[i] = (rand() % (store_max - store_min + 1)) + store_min;
         pthread_mutex_init(&locks[i], NULL);
     }
 
-    //Рандомим количество потребностей у покупателей
+    // Рандомим количество потребностей у покупателей
     for (size_t i = 0; i < CUSTOMER_COUNT; i++)
         customer_demand[i] = (rand() % (customer_max - customer_min + 1)) + customer_min;
 
-    //Создаём потоки покупателей
+    // Создаём потоки покупателей
     for (size_t i = 0; i < CUSTOMER_COUNT; i++)
         pthread_create(&threads[i], NULL, BuyGoods, &thread_id[i]);
 
-    //Создаём поток погрузчика
+    // Создаём поток погрузчика
     pthread_create(&threads[CUSTOMER_COUNT], NULL, LoadShops, &thread_id[CUSTOMER_COUNT]);
 }
 
@@ -190,19 +194,19 @@ int main(void)
 
     Init();
 
-    //Ждём когда потребности покупателей утолятся 
+    // Ждём когда потребности покупателей утолятся
     for (size_t j = 0; j < CUSTOMER_COUNT; j++)
     {
         pthread_join(threads[j], &status);
         printf("Customer %ld\t\t| FINISHED SHOPPING. Exit status %ld\n", j, (long)status);
     }
 
-    //Когда дождались завершившихся потоков с покупателями, выходим из потока погрузчика
+    // Когда дождались завершившихся потоков с покупателями, выходим из потока погрузчика
     pthread_cancel(threads[CUSTOMER_COUNT]);
     pthread_join(threads[CUSTOMER_COUNT], &status);
     printf("LOADING STOPPED\t\t| Loader stopped working.\n");
 
-    //Высвобождаем память
+    // Высвобождаем память
     for (size_t k = 0; k < CUSTOMER_COUNT + 1; k++)
         pthread_mutex_destroy(&locks[k]);
 
--- homework_11/src/spinlock.c
+++ homework_11/src/spinlock.c
@@ -29,55 +29,57 @@ void *BuyGoods(void *arg)
     int thread_id = *(int *)arg;
     while (1)
     {
-        
-
+       // Цикл для прохода покупателя по магазинам, по очерёдно
         for (size_t i = 0; i < STORE_COUNT; i++)
         {
 
-            
-            if (!pthread_spin_trylock(&locks[i])) 
+            // Пытаемся заблокировать магазин
+            if (!pthread_spin_unlock(&locks[i]))
             {
                 printf("Customer %d (%d)\t| store %ld (%d)\t\t| -> ENTER\n", thread_id, customer_demand[thread_id], i, store_supply[i]);
 
+                // Проверяем если в магазине нет припасов, то выходим из него, предварительно разблокировав
                 if (store_supply[i] <= 0)
                 {
                     printf("Customer %d (%d)\t| store %ld (%d)\t\t| <- LEAVE, STORE EMPTY\n", thread_id, customer_demand[thread_id], i, store_supply[i]);
-                    
-                    
-                    pthread_spin_unlock(&locks[i]); 
+                    pthread_spin_unlock(&locks[i]);
+                    // printf("Customer %d\t\t| GOING TO SLEEP zzzZZZ\n", thread_id);
+                    // sleep(2);
                     continue;
                 }
-
-                int temp = store_supply[i];
-                for (int j = 0; j < temp; j++)
+                else
                 {
-                    if (customer_demand[thread_id] <= 0)
+                    /*Если в магазине есть какой нибудь товар, то супаем его/
+                     *Сделал циклом, а не просто вычитанием, хотя бы какую-то полезную нагрузку дать потоку */
+                    int temp = store_supply[i];
+                    for (int j = 0; j < temp; j++)
                     {
-                        pthread_spin_unlock(&locks[i]); 
-                        
-                        return 0;
+                        // Если потребности покупателя удовлетворены, мы разблокируем магазин и выходим из функции
+                        if (customer_demand[thread_id] <= 0)
+                        {
+                            pthread_spin_unlock(&locks[i]);
+                            return 0;
+                        }
+
+                        customer_demand[thread_id]--;
+                        store_supply[i]--;
                     }
 
-                    customer_demand[thread_id]--;
-                    store_supply[i]--;
+                    // Если потребности клиента не были утолены, то разблокируем магазин и идём в другой
+                    printf("Customer %d (%d)\t| store %ld (%d)\t\t| + BUY\n", thread_id, customer_demand[thread_id], i, store_supply[i]);
+                    pthread_spin_unlock(&locks[i]);
                 }
-                printf("Customer %d (%d)\t| store %ld (%d)\t\t| + BUY\n", thread_id, customer_demand[thread_id], i, store_supply[i]);
-                pthread_spin_unlock(&locks[i]); 
-
             }
+            // Если магазин занят, то так и пишем
             else
             {
                 printf("Customer %d\t\t| store %ld\t\t| x STORE BUSY\n", thread_id, i);
             }
-        
-            sleep(2);
+
+            // printf("Customer %d\t\t| GOING TO SLEEP zzzZZZ\n", thread_id);
+            // sleep(2);
         }
     }
-}
-
-void cleanup_mutex_unlock(void *arg)
-{
-    pthread_mutex_unlock((pthread_mutex_t *)arg);
 }
 
 void cleanup_spin_unlock(void *arg)
@@ -93,19 +95,15 @@ void *LoadShops()
         
         if (!pthread_spin_trylock(&locks[store_to_supply])) 
         {
-            
             pthread_cleanup_push(cleanup_spin_unlock, (void *)&locks[store_to_supply]); 
             store_supply[store_to_supply] += LOADER_SUPPLY_AMOUNT;
             printf("Loader\t\t\t| store %d (%d)\t\t| ++ LOAD SUCCESS\n", store_to_supply, store_supply[store_to_supply]);
             pthread_cleanup_pop(1);
-
-            
         }
         else
         {
             printf("Loader\t\t\t| store %d\t| -- LOAD FAIL, STORE BUSY\n", store_to_supply);
         }
-        
         sleep(1);
     }
 }
@@ -114,18 +112,14 @@ void Init()
 {
     srand(time(NULL));
 
-    
     for (size_t i = 0; i < STORE_COUNT; i++)
     {
         store_supply[i] = (rand() % (store_max - store_min + 1)) + store_min;
-        
         pthread_spin_init(&locks[i], PTHREAD_PROCESS_PRIVATE); 
     }
-
     
     for (size_t i = 0; i < CUSTOMER_COUNT; i++)
         customer_demand[i] = (rand() % (customer_max - customer_min + 1)) + customer_min;
-
     
     for (size_t i = 0; i < CUSTOMER_COUNT; i++)
         pthread_create(&threads[i], NULL, BuyGoods, &thread_id[i]);
@@ -151,7 +145,6 @@ int main(void)
     for (size_t k = 0; k < CUSTOMER_COUNT + 1; k++)
         pthread_spin_destroy(&locks[k]); 
                                          
-                                        
     printf("\n\nSPINLOCK\n\n");
 
     return 0;
