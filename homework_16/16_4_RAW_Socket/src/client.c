5204efd85b99450bf3345bc3b9b452866aa38713
shustov-emb <raf_9009@mail.ru>
2026-05-07 16:17:20 +0900

    RAW Сокет

---
+++ homework_16/16_4_RAW_Socket/src/client.c
@@ -0,0 +1,104 @@
+/**
+ * @file client.c
+ * @author Шустов Александр
+ * @brief Простая реализация, принимающего raw сокета
+ * Для проверки работоспособности можно воспользоваться коммандой - echo "Some data" | nc -u 127.0.0.1 777
+ * @version 0.1
+ * @date 2026-05-06
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+#include <sys/socket.h>
+#include <string.h>
+
+int main(void)
+{
+
+    //Создаём raw сокет
+    int raw_fd = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
+    if (raw_fd < 0)
+    {
+        perror("socket error");
+        exit(EXIT_FAILURE);
+    }
+
+    //Данные для приёма пакетов
+    struct sockaddr_in sender_addr;
+    socklen_t addr_size;
+    char buffer[4096];
+
+    size_t recieved_packets = 0;
+
+    printf("\n");
+
+    while (recieved_packets < 10)
+    {
+        //Обнуляем буфер, на всякий случай
+        memset(buffer, 0, sizeof(buffer));
+        
+        //Получаем пакет
+        addr_size = sizeof(sender_addr);
+        int recv_bytes = recvfrom(raw_fd, buffer, sizeof(buffer), 0, (struct sockaddr *)&sender_addr, &addr_size);
+        if (recv_bytes < 0)
+        {
+            perror("recvfrom error");
+            continue;
+        }
+
+        struct iphdr *ip_header = (struct iphdr *)buffer;
+        //ihl показывает длину ip заголовка, в int обычно в 32 битных словах
+        //Надо перевести в байты
+        int ip_header_length = ip_header->ihl * 4;
+
+        //Если полученное количество байт меньше чем хотя бы два необходимых заголовка, то пропускаем пакет
+        if (recv_bytes < ip_header_length + (int)sizeof(struct udphdr))
+        {
+            printf("recv bytes less than ip_header");
+            continue;
+        }
+
+        //Достаём данные, смешаем начало буфера на длины ip и udp заголовка, udp заголовок насколько я понял - стандарный - 8 байт
+        //Поэтому его не нужно вычислять как длину ip заголовка, которая может варьироваться
+        char *data = (char *)(buffer + ip_header_length + sizeof(struct udphdr));
+        
+        //Из всего пакета, мы вычитаем заголовки, это и будет длинна наших данных
+        int data_len = ntohs(ip_header->tot_len) - ip_header_length - sizeof(struct udphdr);
+
+        struct in_addr src_addr, dest_addr;
+        src_addr.s_addr = ip_header->saddr;
+        dest_addr.s_addr = ip_header->daddr;
+
+        char src_str[INET_ADDRSTRLEN];
+        char dest_str[INET_ADDRSTRLEN];
+        // Переводим адреса в удобоваримый вариант, пользуемся удобным определением INET_ADDRSTRLEN
+        inet_ntop(AF_INET, &src_addr, src_str, INET_ADDRSTRLEN);
+        inet_ntop(AF_INET, &dest_addr, dest_str, INET_ADDRSTRLEN);
+
+        //Из udp заголовка можно достать порты назначения, и источника
+        struct udphdr *udp_header = (struct udphdr *)(buffer + ip_header_length);
+
+        //Выводим пакет, но мало какие пакеты мы сможем прочитать, мало кто строками информацию отправляет
+        //Но для теста нагуглил команду: echo "Some data" | nc -u 127.0.0.1 777 
+        //Можно на адресс и порт послать свои udp данные 
+        printf("%-10s: %s:%d -> %s:%d\n","Packet",
+               src_str, ntohs(udp_header->source),
+               dest_str, ntohs(udp_header->dest));
+        printf("%-10s: %d\n","Data len", data_len);
+        printf("%-10s: %.*s\n\n","Payload", data_len, data);
+
+        //По новой вспомнил про форматированный красивый вывод, надо бы не забывать пользоваться им
+
+        recieved_packets++;
+    }
+
+    // printf("\n");
+    exit(EXIT_SUCCESS);
+}
